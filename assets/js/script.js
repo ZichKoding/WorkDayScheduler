7e04f7ca619d19fbeea73c364ad288604d86525c
Chris Zichko <chriszichkocoding@gmail.com>
2021-09-26 11:17:21 -0400

    Have now officially automated everything for interactions with local storage and automated the day and textarea background color

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,55 +13,121 @@ let hourTextArea = {
     "#hourFive": 17,
 };
 
-console.log(hourOfDay);
-
 
 // Determining and displaying the day of the week in the header.
-switch(dayOfWeek){
-    case 1:
-        $("#currentDay").text("Monday");
-        break;
+function displayDay() {
+    switch(dayOfWeek){
+        case 1:
+            $("#currentDay").text("Monday");
+            break;
 
-    case 2:
-        $("#currentDay").text("Tuesday");
-        break;
+        case 2:
+            $("#currentDay").text("Tuesday");
+            break;
 
-    case 3:
-        $("#currentDay").text("Wednesday");
-        break;
+        case 3:
+            $("#currentDay").text("Wednesday");
+            break;
 
-    case 4:
-        $("#currentDay").text("Thursday");
-        break;
-    
-    case 5:
-        $("#currentDay").text("Friday");
-        break;
+        case 4:
+            $("#currentDay").text("Thursday");
+            break;
+        
+        case 5:
+            $("#currentDay").text("Friday");
+            break;
 
-    case 6:
-        $("#currentDay").text("Saturday");
-        break;
+        case 6:
+            $("#currentDay").text("Saturday");
+            break;
 
-    case 7:
-        $("#currentDay").text("Sunday");
-        break;
+        case 0:
+            $("#currentDay").text("Sunday");
+            break;
 
-    default:
-        $("#currentDay").text("Wonder What Day It Is?!..... Me too...");
-}
+        default:
+            $("#currentDay").text("Wonder What Day It Is?!..... Me too...");
+    }
+};
 
 // loop through the body container and applying the correct background to the schedule hours
-for (let key in hourTextArea) {
-    let hourVal = hourTextArea[key];
-    
-    // setting current hour block red.
-    if (hourOfDay === hourVal) {
-        $(key).addClass("present");
-    } 
-    else if (hourOfDay > hourVal) {
-        $(key).addClass("past");
-    }
-    else {
-        $(key).addClass("future");
+function textBackChange() {
+    for (let key in hourTextArea) {
+        let hourVal = hourTextArea[key];
+        
+        // setting current hour block red.
+        if (hourOfDay === hourVal) {
+            $(key).addClass("present");
+        } 
+        else if (hourOfDay > hourVal) {
+            $(key).addClass("past");
+        }
+        else {
+            $(key).addClass("future");
+        }
     }
 }
+
+// load data from localStorage
+function loadingData() {
+    if (localStorage.length > 0){
+        for (let i = 0; i < localStorage.length; i++) {
+            for (let key in hourTextArea) {
+                let textVal = hourTextArea[key];
+                let localItem = localStorage.getItem(textVal);
+                if (localItem) {
+                    localItem = localItem.replace('"', ``);
+                    localItem = localItem.replace('"', '');
+                    $(key).text(localItem);
+                }
+            }
+        }
+    } else {
+        console.log("It seems you've never been here before.......... mwhahaha")
+    }
+};
+
+// Save button logic
+$("#nineAM").click(function () {
+    localStorage.setItem(9, JSON.stringify($("#hourNine").val()));
+});
+
+$("#tenAM").click(function () {
+    localStorage.setItem(10, JSON.stringify($("#hourTen").val()));
+});
+
+$("#elevenAM").click(function () {
+    localStorage.setItem(11, JSON.stringify($("#hourEleven").val()));
+});
+
+$("#twelvePM").click(function () {
+    localStorage.setItem(12, JSON.stringify($("#hourTwelve").val()));
+});
+
+$("#onePM").click(function () {
+    localStorage.setItem(13, JSON.stringify($("#hourOne").val()));
+});
+
+$("#twoPM").click(function () {
+    localStorage.setItem(14, JSON.stringify($("#hourTwo").val()));
+});
+
+$("#threePM").click(function () {
+    localStorage.setItem(15, JSON.stringify($("#hourThree").val()));
+});
+
+$("#fourPM").click(function () {
+    localStorage.setItem(16, JSON.stringify($("#hourFour").val()));
+});
+
+$("#fivePM").click(function () {
+    localStorage.setItem(17, JSON.stringify($("#hourFive").val()));
+});
+
+
+// interval loop to make sure everything is keeping updated every second.
+setInterval(() => {
+    displayDay();
+    loadingData();
+    textBackChange();
+}, 1000);
